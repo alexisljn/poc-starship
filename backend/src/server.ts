602ce1fff8c5555c08e357d335605524559c4941
alexisljn <alexis.lejeune@gmx.fr>
2024-05-27 19:25:26 +0200

    Initialize SocketEnvine at app bootstrap

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import {createServer} from "http";
+import websocket from "./websocket";
+
 
 const app = express();
 
+const httpServer = createServer(app);
+
 const port = 3000;
+
+websocket(httpServer);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+httpServer.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
