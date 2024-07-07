5abb5b642469532285332a60c083f302a49aaed4
alexisljn <alexis.lejeune@gmx.fr>
2024-07-07 21:27:47 +0200

    Rewrite store to handle multiple canvas

--- backend/src/store.ts
+++ backend/src/store.ts
@@ -1,15 +1,25 @@
-import {PaintCoords} from "./types";
+import {Cell, CellWithCanvasId} from "./types";
 
-let store: PaintCoords[] = [];
+let store: Record<string, Cell[]> = {};
 
 export function getStore() {
     return store;
 }
 
-export function addToStore(coords: PaintCoords) {
-    store.push(coords);
+export function addToStore(cellData: CellWithCanvasId) {
+    const {x, y, color, canvasId} = cellData;
+
+    if (store[canvasId] === undefined) {
+        store[canvasId] = [];
+    }
+
+    store[canvasId].push({
+        x,
+        y,
+        color
+    });
 }
 
-export function clearStore() {
-    store = [];
+export function clearStore(canvasId: string) {
+    delete store[canvasId];
 }
