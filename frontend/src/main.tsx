1c2fad38414734225c56fa8517b588fe1672cae5
alexisljn <alexis.lejeune@gmx.fr>
2024-07-07 21:21:18 +0200

    Get rid of createBrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                path: "view",
-                element: <App/>
-            }
-        ]
-    }
-])
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-   <RouterProvider router={router}/>
+  <App/>
 )
